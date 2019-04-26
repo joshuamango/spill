e087fe949830184b43200dd82051e0db4e3e7920
joshuamango <aodeyem2@students.kennesaw.edu>
2019-04-25 21:26:02 -0400

    Add deletion and retrieval

--- database.js
+++ database.js
@@ -1,85 +1,60 @@
 const sqlite3 = require('sqlite3').verbose();
 
-function addName(name) {
-    //Open the database
-    const db = new sqlite3.Database('spillsave', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Connected to the spillsave database.');
-    });
+function addName(user, pass) {
+  //Open the database
+  const db = new sqlite3.Database('spillsave', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the spillsave database.');
+  });
 
-    // Perform actions
-    db.serialize(() => {
-        const write = db.prepare('INSERT INTO user VALUES (?)');
-        write.run(name);
-        write.finalize();
-    })
+  // Perform actions
+  db.serialize(() => {
+    const write = db.prepare('INSERT INTO USER(Username, Password) VALUES (?, ?)');
+    write.run(user, pass);
+    write.finalize();
+  })
 
-    //Close the database
-    db.close((err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Closed the database connection.');
-    })
+  //Close the database
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Closed the database connection.');
+  })
 }
 
-function deleteName(name) {
-    // Open the database
-    const db = new sqlite3.Database('spillsave', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Connected to the spillsave database.');
-    });
+function deleteNote(noteKey) {
+  console.log("KEY FROM DATABASE: " + noteKey);
+  // Open the database
+  const db = new sqlite3.Database('spillsave', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the spillsave database.');
+  });
 
-    // Perform actions
-    db.run('DELETE FROM name WHERE name=?', name, function(err) {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log(`Row(s) deleted ${this.changes}`);
-    })
+  // Perform actions
+  db.run('DELETE FROM NOTES WHERE key=?', noteKey, function (err) {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log(`Row(s) deleted ${this.changes}`);
+  })
 
-    // Close the database
-    db.close((err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        console.log('Closed the database connection.');
-    })
+  // Close the database
+  db.close((err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Closed the database connection.');
+  })
 }
 
 function getNames(callback) {
-    const names = [];
-    // Open the database
-    const db = new sqlite3.Database('spillsave', sqlite3.OPEN_READONLY, (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Connected to the spillsave database.');
-    });
+  const names = [];
 
-    // Perform actions
-    db.serialize(() => {
-        db.each("SELECT name FROM user", (err, row) => {
-            names.push(row.name);
-        });
-    });
-    
-    // Close the database
-    db.close((err) => {
-        if (err) {
-            console.error(err.message);
-        }
-        console.log('Closed the database connection.');
-        callback(names);
-    })
-}
-
-function login(name, callback) {
-  let doesExist = false;
   // Open the database
   const db = new sqlite3.Database('spillsave', sqlite3.OPEN_READONLY, (err) => {
     if (err) {
@@ -91,8 +66,36 @@ function login(name, callback) {
   // Perform actions
   db.serialize(() => {
     db.each("SELECT name FROM user", (err, row) => {
-      if (row.name == name) {
-        doesExist = true; break;
+      names.push(row.name);
+    });
+  });
+
+  // Close the database
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Closed the database connection.');
+    callback(names);
+  })
+}
+
+function login(user, pass, callback) {
+  let doesExist = false;
+
+  // Open the database
+  const db = new sqlite3.Database('spillsave', sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the spillsave database.');
+  });
+
+  // Perform actions
+  db.serialize(() => {
+    db.each("SELECT Username, Password FROM USER", (err, row) => {
+      if (row.Username == user && row.Password == pass) {
+        doesExist = true;
       }
     })
   })
@@ -108,6 +111,71 @@ function login(name, callback) {
   })
 }
 
+function saveNote(username, title, body, time, key) {
+  // Open the database
+  const db = new sqlite3.Database('spillsave', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the spillsave database.');
+  });
+
+  // Insert name and message into the "notes" table
+  db.serialize(() => {
+    const writeName = db.prepare('INSERT INTO NOTES(Username, Title, Body, Time, Key) VALUES(?, ?, ?, ?, ?)');
+    writeName.run(username, title, body, time, key);
+    writeName.finalize();
+  })
+
+  // Close the database
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Closed the database connection.');
+  })
+}
+
+function getNotes(name, callback) {
+  let messages = [];
+
+  // Open the database
+  const db = new sqlite3.Database('spillsave', sqlite3.OPEN_READONLY, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the spillsave database.');
+  });
+
+  // Retrieve all notes written by the current user
+  db.serialize(() => {
+    db.each("SELECT Username, Title, Body, Time, Key FROM NOTES", (err, row) => {
+      if (row.Username == name) {
+        messages.push({
+          username: row.Username,
+          title: row.Title,
+          body: row.Body,
+          time: row.Time,
+          key: row.Key
+        });
+        console.log(row);
+      }
+    })
+  })
+
+  // Close the database
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Closed the database connection.');
+    callback(messages);
+  })
+}
+
 module.exports.login = login;
 module.exports.addName = addName;
 module.exports.getNames = getNames;
+module.exports.saveNote = saveNote;
+module.exports.getNotes = getNotes;
+module.exports.deleteNote = deleteNote;
--- server.js
+++ server.js
@@ -8,26 +8,50 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
+let currentUser = '';
 
 app.get('/api/names', (req, res) => {
-  db.getNames(function(names) {
-    res.json({names: names});
+  db.getNames(function (names) {
+    res.json({ names: names });
   });
-})
+});
 
-app.post('/api/world', (req, res) => {
-  db.addName(req.body.post);
-  res.send(`I received your POST request. This is what you sent me: ${req.body.post}`);
+app.get('/api/currentUser', (req, res) => {
+  res.json({ currentUser: currentUser })
+});
+
+app.post('/api/addUser', (req, res) => {
+  db.addName(req.body.username, req.body.password);
+  currentUser = req.body.username;
+  res.send(`I received your POST request. This is what you sent me: ${req.body.username} ${req.body.password}`);
 });
 
 app.post('/api/login', (req, res) => {
-  console.log(`req.body.post: ${req.body.post}`)
-  db.login(req.body.post, function(loginResult) {
-    res.json({login: loginResult});
+  currentUser = req.body.username;
+  db.login(req.body.username, req.body.password, function (loginResult) {
+    res.json({ login: loginResult });
   });
+});
+
+app.post('/api/savenote', (req, res) => {
+  console.log("Title: " + req.body.first)
+  console.log("Body: " + req.body.second)
+  console.log("Time: " + req.body.time)
+  console.log("Key: " + req.body.key)
+  db.saveNote(currentUser, req.body.first, req.body.second, req.body.time, req.body.key);
+});
+
+app.get('/api/getnotes', (req, res) => {
+  console.log(`Current User: ${currentUser}`)
+  db.getNotes(currentUser, function (array) {
+    res.json({ list: array })
+    console.log(array);
+  });
+});
+
+app.post('/api/delete', (req, res) => {
+  console.log("Key: " + req.body.key);
+  db.deleteNote(req.body.key);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
